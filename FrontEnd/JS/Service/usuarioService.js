8645b03107da552f453a3b64a86ebdbdb6431404
Unknown <camila.batista@cwi.com.br>
2017-07-08 15:17:25 -0300

    feat(Front): adicionar metodos na service de usuario
    metodos:
    obterCameras | adicionarCameras | obterVeiculos | obterVeiculoPlaca | obterVeiculoClonado | adicionarVeiculo | obterRegistro|  adicionarRegistro |obterCidades |adicionarCidades

--- FrontEnd/JS/Service/usuarioService.js
+++ FrontEnd/JS/Service/usuarioService.js
@@ -7,7 +7,17 @@ app.service('usuarioService', function($http){
         obterPorRole: obterPorRole,
         obterPorId: obterPorId,
         adicionar: adicionar,
-        update: update
+        update: update,
+        obterCameras: obterCameras,
+        adicionarCameras: adicionarCameras,
+        obterVeiculos: obterVeiculos,
+        obterVeiculoPlaca: obterVeiculoPlaca,
+        obterVeiculoClonado: obterVeiculoClonado,
+        adicionarVeiculo: adicionarVeiculo,
+        obterRegistro: obterRegistro,
+        adicionarRegistro: adicionarRegistro,
+        obterCidades: obterCidades,
+        adicionarCidades: adicionarCidades        
     }
 
     function obter(){
@@ -15,11 +25,11 @@ app.service('usuarioService', function($http){
     }
 
     function obterPorRole(role){
-        return $http.get(url + 'role?role=' + role);
+        return $http.get(url + '/role?role=' + role);
     }
 
     function obterPorId(id){
-        return $http.get(url + 'id?id=' + id);
+        return $http.get(url + '/id?id=' + id);
     }
 
     function adicionar(usuario){
@@ -28,5 +38,45 @@ app.service('usuarioService', function($http){
 
     function update(usuario){
         return $http.post(url, usuario);
+    }
+
+    function obterCameras(){
+        return $http.get(url + '/cameras');
+    }
+
+    function adicionarCameras(cameras){
+        return $http.post(url, cameras);
+    }
+
+    function obterVeiculos(){
+        return $http.get(url + '/veiculos');
+    }
+
+    function obterVeiculoPlaca(placa){
+        return $http.get(url + '/veiculo/placa');
+    }
+
+    function obterVeiculoClonado(placa){
+        return $http.get(url + '/veiculo/clonado');
+    }
+
+    function adicionarVeiculo(veiculo){
+        return $http.post(url, veiculo);
+    }
+
+    function obterRegistro(){
+        return $http.get(url + '/registros');
+    }
+
+    function adicionarRegistro(registro){
+        return $http.post(url, registro);
+    }
+
+    function obterCidades(){
+        return $http.get(url + '/cidades');
+    }
+
+    function adicionarCidades(cidade){
+        return $http.post(url, cidade);
     }
 });
