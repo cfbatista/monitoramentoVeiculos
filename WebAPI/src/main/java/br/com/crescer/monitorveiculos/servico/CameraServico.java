119afaaaee51dc0ae50045fa0d75dc6e88dd9bb6
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-07-09 03:16:54 -0300

    feat(BackEnd): primeira partes das métodos com querys.

--- WebAPI/src/main/java/br/com/crescer/monitorveiculos/servico/CameraServico.java
+++ WebAPI/src/main/java/br/com/crescer/monitorveiculos/servico/CameraServico.java
@@ -1,5 +1,6 @@
 package br.com.crescer.monitorveiculos.servico;
 
+import br.com.crescer.monitorveiculos.entidade.Camera;
 import br.com.crescer.monitorveiculos.repositorio.CameraRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,9 @@ public class CameraServico {
 
     @Autowired
     private CameraRepositorio cameraRepositorio;
+
+    public Camera findById(long id) {
+        return cameraRepositorio.findOne(id);
+    }
 
 }
