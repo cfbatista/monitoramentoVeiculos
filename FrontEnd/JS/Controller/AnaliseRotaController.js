5a79d35977ec00bd31ca0c6462db6428a491406e
William Gonçalves <william.goncalves@cwi.com.br>
2017-07-10 11:32:06 -0300

    Feat(Front End): Ajustando tela de mapas e buscando cameras para plotar no front com possibilidade de traçar rotas

--- FrontEnd/JS/Controller/AnaliseRotaController.js
+++ FrontEnd/JS/Controller/AnaliseRotaController.js
@@ -7,21 +7,6 @@ app.controller('analiseRotaController', function ($scope, $routeParams, $locatio
         }).catch(error => console.log(error));
     }
 
-    //radares
-    var locations = [
-        [-29.658623, -51.140586, "Radar 1"],
-        [-29.658511, -51.140428, "Radar 2"],
-        [-29.680632, -51.142045, "Radar 3"],
-        [-29.680623, -51.142222, "Radar 4"],
-        [-29.714803, -51.147935, "Radar 5"],
-        [-29.736532, -51.149887, "Radar 6"],
-        [-29.736495, -51.150129, "Radar 7"],
-        [-29.75971, -51.148261, "Radar 8"],
-        [-29.759716, -51.148331, "Radar 9"],
-        [-29.758328, -51.145979, "Radar 10"],
-        [-29.758403, -51.146017, "Radar 11"]
-    ];
-
     //pontos de calor
 
     var heatMapData = [
@@ -38,7 +23,7 @@ app.controller('analiseRotaController', function ($scope, $routeParams, $locatio
 
     $scope.partida;
     $scope.chegada;
-    $scope.radares = locations;
+    //$scope.radares = locations;
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var directionsService = new google.maps.DirectionsService;
     var NovoHamburgo = new google.maps.LatLng(-29.6918991, -51.1255697);
@@ -81,8 +66,8 @@ app.controller('analiseRotaController', function ($scope, $routeParams, $locatio
 
     //funcao para calcular a rota
     function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        var start = $scope.partida[0] + ',' + $scope.partida[1];
-        var end = $scope.chegada[0] + ',' + $scope.chegada[1];
+        var start = $scope.partida.latitude + ',' + $scope.partida.longitude ;
+        var end = $scope.chegada.latitude + ',' + $scope.chegada.longitude;
         directionsService.route({
             origin: start,
             destination: end,
