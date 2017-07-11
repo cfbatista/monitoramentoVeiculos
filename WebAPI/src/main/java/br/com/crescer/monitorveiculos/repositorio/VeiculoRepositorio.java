63afc8358fe213c8517d50b7aea261e7b7328512
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-07-11 11:32:24 -0300

    fix(BackEnd): ajuste para método heatmap.

--- WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/VeiculoRepositorio.java
+++ WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/VeiculoRepositorio.java
@@ -3,6 +3,7 @@ package br.com.crescer.monitorveiculos.repositorio;
 import br.com.crescer.monitorveiculos.entidade.RetornoHeatMapModel;
 import br.com.crescer.monitorveiculos.entidade.Veiculo;
 import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -15,14 +16,8 @@ public interface VeiculoRepositorio extends CrudRepository<Veiculo, Long> {
 
     public Veiculo findByPlaca(String placa);
 
-//    @Query("select new br.com.crescer.monitorveiculo.entidade.RetornoHeatMapModel(ca.idcamera, ca.latitude, ca.longitude, ca.nome,"
-//            + " trunc(re.data_hora), "
-//            + " count(re.idregistro))"
-//            + " from camera ca"
-//            + " inner join registro re on re.idcamera = ca.idcamera"
-//            + "where re.data_hora between to_date(:dataInicial, 'dd/mm/yyyy') and to_date(:dataFinal, 'dd/mm/yyyy') + .99999"
-//            + "and re.direcao = :direcao"
-//            + "group by ca.idcamera, ca.nome, trunc(re.data_hora), ca.latitude, ca.longitude;")
-//    public RetornoHeatMapModel retornarModel(@Param("dataIncial") Date dataInicial, @Param("dataFinal") Date dataFinal, @Param("direcao") String direcao);
-
+    @Query("SELECT new br.com.crescer.monitorveiculos.entidade.RetornoHeatMapModel(re.camera, TRUNC(re.dataHora), COUNT(re.idregistro)) FROM"
+            + " Registro re WHERE re.dataHora BETWEEN :dataInicial AND :dataFinal"
+            + " GROUP BY re.camera, TRUNC(re.dataHora)")
+    List<RetornoHeatMapModel> retornarModel(@Param("dataInicial") Date dataInicial, @Param("dataFinal") Date dataFinal);
 }
