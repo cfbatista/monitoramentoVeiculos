8022f5305b719653b2312fe7231dcc811c3446d1
Unknown <camilafreitasbatista@gmail.com>
2017-07-11 15:30:59 -0300

    feat(BackEnd): adicionar metodos de consulta de veiculo

--- WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/CidadeRepositorio.java
+++ WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/CidadeRepositorio.java
@@ -3,6 +3,7 @@ package br.com.crescer.monitorveiculos.repositorio;
 import br.com.crescer.monitorveiculos.entidade.Cidade;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
@@ -12,4 +13,7 @@ public interface CidadeRepositorio extends CrudRepository<Cidade, Long> {
     
    @Query("select count(distinct ci.idcidade) from Camera ca inner join ca.idcidade ci") 
    public Long getNumeroCidadesComRegistros(); 
+   
+   @Query("SELECT nome from cidade inner join veiculo on cidade.idcidade = :cidadeVeiculo")
+   Cidade getCidadeDoVeiculo(@Param("cidadeVeiculo") Long cidadeVeiculo);
 }
--- WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/VeiculoRepositorio.java
+++ WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/VeiculoRepositorio.java
@@ -1,5 +1,7 @@
-package br.com.crescer.monitorveiculos.repositorio;
+    package br.com.crescer.monitorveiculos.repositorio;
 
+import br.com.crescer.monitorveiculos.entidade.ConsultaVeiculosModel;
+import br.com.crescer.monitorveiculos.entidade.RetornoHeatMapModel;
 import br.com.crescer.monitorveiculos.entidade.Veiculo;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,5 +11,14 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface VeiculoRepositorio extends CrudRepository<Veiculo, Long> {
 
-    public Veiculo findByPlaca(String placa);
+    public Veiculo findByPlacaIgnoreCase(String placa);
+
+    @Query("SELECT new br.com.crescer.monitorveiculos.entidade.RetornoHeatMapModel(re.camera, TRUNC(re.dataHora), COUNT(re.idregistro)) FROM"
+            + " Registro re WHERE re.dataHora BETWEEN :dataInicial AND :dataFinal"
+            + " GROUP BY re.camera, TRUNC(re.dataHora)")
+    List<RetornoHeatMapModel> retornarModel(@Param("dataInicial") Date dataInicial, @Param("dataFinal") Date dataFinal);
+
+    
+//    @Query("SELECT new br.com.crescer.monitorveiculos.entidade.ConsultaVeiculosModel()")
+//    List<ConsultaVeiculosModel> consultaVeiculos(@Param("placa") String placa);;
 }
