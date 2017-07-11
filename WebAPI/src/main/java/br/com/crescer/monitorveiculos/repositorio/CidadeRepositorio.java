3de08c5ea32afe3f74882711c987571f2c8103e3
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-07-11 17:04:21 -0300

    fix(BackEnd): ajustar métodos de cidade.

--- WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/CidadeRepositorio.java
+++ WebAPI/src/main/java/br/com/crescer/monitorveiculos/repositorio/CidadeRepositorio.java
@@ -3,17 +3,14 @@ package br.com.crescer.monitorveiculos.repositorio;
 import br.com.crescer.monitorveiculos.entidade.Cidade;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 /**
  *
  * @author Diandra Rocha
  */
 public interface CidadeRepositorio extends CrudRepository<Cidade, Long> {
-    
-   @Query("select COUNT(distinct ci.idcidade) FROM Camera ca INNER JOIN ca.idcidade ci") 
-   public Long getNumeroCidadesComRegistros(); 
-   
-//   @Query("SELECT c.nome FROM Cidade c INNER JOIN Veiculo WHERE cidade.idcidade = :cidadeVeiculo")
-//   Cidade getCidadeDoVeiculo(@Param("cidadeVeiculo") Long cidadeVeiculo);
+
+    @Query("select count(distinct ci.idcidade) from Camera ca inner join ca.idcidade ci")
+    public Long getNumeroCidadesComRegistros();
+
 }
