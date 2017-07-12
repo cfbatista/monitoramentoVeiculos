a3ef0bc5d6fbebf656fa836ea543350d60a224eb
Unknown <camilafreitasbatista@gmail.com>
2017-07-12 17:22:31 -0300

    refactory(front): alterar gráficos da pagina inicial

--- FrontEnd/JS/Controller/graficoController.js
+++ FrontEnd/JS/Controller/graficoController.js
@@ -1,80 +1,26 @@
 app.controller('graficoController', function($scope, $http, graficoService, $routeParams, $location, authService, authConfig, toastr) {
-  
-  
-  // chart data source
-  $scope.graficoTotalCidades = {
-    "chart": {
-      "caption": "Total de cidades com monitoramento",
-      "captionFontSize": "20",
-      // more chart properties - explained later
-    },
-    "data": [{
-        "label": "Figura 1",
-        "value": totalCidades()
-      }, //more chart data
-    ]
-  };
 
-  $scope.graficoTotalVeiculos = {
-      "chart": {
-          "caption": "Total de veículos",
-          "captionFontSize": "20",
-          "theme": "zune",
-        "palettecolors": "#0075c2"
-      },
-      "data": [{
-          "label": "Figura 2",
-          "value": totalVeiculos()
-      }]
-  };
-
-//   $scope.graficoClonados = {
-//       "chart": {
-//           "caption": "Total de veículos clonados",
-//           "captionFontSize": "20"
-//       },
-//       "data": [{
-//           "label": "Figura 3",
-//           "value": totalVeiculosClonados()
-//       }]
-//   };
-
-  $scope.graficoUltimasOcorrencias = {
-      "chart": {
-          "caption" : "Total de ocorrêcias nos últimos 3 meses",
-          "captionFontSize": "20"
-      }, 
-      "data": [{
-          "label": "Figura 4",
-          "value": ocorrenciasUltimos3meses()
-      }]
-  };
-
-  
-    
-
+    totalCidades();
     function totalCidades(){
         graficoService.obterCidades().then(response => {
-            console.log(response.data)
-            $scope.graficoTotalCidades.data[0].value = response.data;
-            //$scope.totalCidade = response.data;
+            $scope.totalCidade = response.data;
         }).catch(error => console.log(error));   
     }
 
+    totalVeiculos();
     function totalVeiculos(){
         graficoService.totalVeiculos().then(response => {
-            console.log(response.data);
-            $scope.graficoTotalVeiculos.data[0].value = response.data;
+             $scope.totalVeiculo = response.data;
         }).catch(error => console.log(error));
     }
-
+   
+    ocorrenciasUltimos3meses();
     function ocorrenciasUltimos3meses(){
         graficoService.ocorrenciasUltimos3meses().then(response => {
-            console.log(response.data);
-            $scope.graficoUltimasOcorrencias.data[0].value = response.data;
+            $scope.ocorrencias = response.data;
         }).catch(error => console.log(error));
     }
-
+    
     // function totalVeiculosClonados(){
     //     graficoService.totalVeiculosClonados().then(response => {
     //         $scope.totalClonado.data[0] = response.data.length;
