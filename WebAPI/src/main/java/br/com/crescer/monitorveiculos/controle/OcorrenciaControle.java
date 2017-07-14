c717d9156f45154dd861459a3b5ff46e8b97918f
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-07-14 11:58:57 -0300

    fix(BackEnd): impor de @Secured.

--- WebAPI/src/main/java/br/com/crescer/monitorveiculos/controle/OcorrenciaControle.java
+++ WebAPI/src/main/java/br/com/crescer/monitorveiculos/controle/OcorrenciaControle.java
@@ -6,6 +6,7 @@ import br.com.crescer.monitorveiculos.servico.OcorrenciaServico;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
