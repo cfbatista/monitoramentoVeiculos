0d1ebca9a791ac90fe44940910f87d22b07b9845
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-07-14 02:31:42 -0300

    feat(FrontEnd): service de camera.

--- FrontEnd/JS/Service/cameraService.js
+++ FrontEnd/JS/Service/cameraService.js
@@ -1,4 +1,4 @@
-app.service('cameraService', function ($http) {
+app.service('cameraService', function($http) {
 
     var url = 'http://localhost:9090/camera';
 
@@ -6,7 +6,47 @@ app.service('cameraService', function ($http) {
         return $http.post(url + '/obter/contagemdiassemana', model);
     }
 
+    function contagemTotal() {
+        return $http.get(url + 'obter/contagemtotal');
+    }
+
+    function obterTodasCameras() {
+        return $http.get(url + 'obter/todascameras');
+    }
+
+    function obterPorId(id) {
+        return $http.get(url + '/obterporid/' + id);
+    }
+
+    function calculoEnergia(RegistroCountModel) {
+        return $http.post(url + '/energia', RegistroCountModel)
+    }
+
+    function obterContagemDiaSemana(DataModel) {
+        return $http.post(url + '/obter/contagemdiassemana', DataModel);
+    }
+
+    function obterContagemRegistrosPorRota(RegistroCountModel) {
+        return $http.post(url + '/obter/numeroregistros', RegistroCountModel);
+    }
+
+    function obterVelocidadeMedia(RegistroCountModel) {
+        return $http.post(url + '/obter/mediavelocidade', RegistroCountModel);
+    }
+
+    function obterPlacasAcimaVelocidade(idCamera) {
+        return $http.get(url + '/obter/foramaximovelocidade/' + idCamera);
+    }
+
     return {
-        buscarRegistroVeiculosPorData: buscarRegistroVeiculosPorData
+        buscarRegistroVeiculosPorData: buscarRegistroVeiculosPorData,
+        contagemTotal: contagemTotal,
+        obterTodasCameras: obterTodasCameras,
+        obterPorId: obterPorId,
+        calculoEnergia: calculoEnergia,
+        obterContagemDiaSemana: obterContagemDiaSemana,
+        obterContagemRegistrosPorRota: obterContagemRegistrosPorRota,
+        obterVelocidadeMedia: obterVelocidadeMedia,
+        obterPlacasAcimaVelocidade: obterPlacasAcimaVelocidade
     }
 });
