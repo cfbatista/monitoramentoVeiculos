c66374a752f20b3b0b42761b878ca9a1591a6b28
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-07-14 09:33:56 -0300

    feat(BackEnd): métodos para realizar auditoria.

---
+++ WebAPI/src/main/java/br/com/crescer/monitorveiculos/entidade/Auditoria.java
@@ -0,0 +1,59 @@
+package br.com.crescer.monitorveiculos.entidade;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author diandra.rocha
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "AUDITORIA")
+
+public class Auditoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private static final String SEQ_NAME = "SQ_AUDITORIA";
+
+    @GeneratedValue(strategy = SEQUENCE, generator = SEQ_NAME)
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME, allocationSize = 1)
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "IDAUDITORIA")
+    private Long idauditoria;
+
+    @Size(max = 255)
+    @Column(name = "TIPOCONSULTA")
+    private String tipoconsulta;
+
+    @JoinColumn(name = "IDUSUARIO", referencedColumnName = "IDUSUARIO")
+    @ManyToOne
+    private Usuario usuario;
+
+    @Column(name = "DADOCONSULTADO")
+    private String dadoconsultado;
+
+}
